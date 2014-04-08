288b600160dc43440280a57b06b29e731f633c7c
barry528 <smamadou.barry.87@gmail.com>
2014-04-08 12:11:42 +0200

    ref : yes

--- Main.java
+++ Main.java
@@ -3,8 +3,8 @@
 public class Main{
 
 	public static void main(String[] args){
-		System.out.print("Hello worlds");
-		System.out.println(" Every Body1");
+		System.out.print("Hello world");
+		//System.out.println(" Every Body");
 		
 	}
 }
