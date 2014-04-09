e403b0f2a74f5ce6183a6bf7604bac60d592f9bc
barry528 <smamadou.barry.87@gmail.com>
2014-04-09 13:49:04 +0200

    [master]: that's it

--- Main.java
+++ Main.java
@@ -5,8 +5,6 @@ public class Main{
 		public static void main(String[] args){
 		Calc c= new Calc();
 		System.out.print("Hello world !");
-		System.out.print("Somme : "+c.mult(3,5));
-
 
 		System.out.println("Somme :"+c.somme(3,5));
 		
