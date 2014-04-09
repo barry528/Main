a26a84ca87c98bb22f42ea9c25d23d7916de91f1
barry528 <smamadou.barry.87@gmail.com>
2014-04-09 16:43:35 +0200

    [master]: un commit

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class Main{
 
 		public static void main(String[] args){
 		Calc c= new Calc();
-		System.out.print("  !");
+		System.out.print(" ok !");
 
 		System.out.println("Multiplication :"+c.somme(3,5));
 		
