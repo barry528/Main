248f53690c3dc1b6bd652bcc7fbca5ab8360ac43
barry528 <smamadou.barry.87@gmail.com>
2014-04-09 11:44:46 +0200

    ok

---
+++ Calc.java
@@ -0,0 +1,12 @@
+
+
+public class Calc{
+
+public int somme(int a, int b){
+	return a+b+1;
+} 
+public int mult(int a, int b){
+	return a*b;
+} 
+
+}
--- Main.java
+++ Main.java
@@ -3,7 +3,11 @@
 public class Main{
 
 	public static void main(String[] args){
-		System.out.print("Hello World");
+		public static void main(String[] args){
+		Calc c= new Calc();
+		System.out.print("Somme : "+c.mult(3,5));
+		
+	}
 		
 	}
 }
