3e7f0c4174e2d79fb69b29f2b378b9461fbba03a
barry528 <smamadou.barry.87@gmail.com>
2014-04-09 13:48:13 +0200

    [master]: that's it

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class Main{
 
 		public static void main(String[] args){
 		Calc c= new Calc();
-		System.out.print("Salut tout le monde je sais faire des multiplications !");
+		System.out.print("Hello world !");
 		System.out.print("Somme : "+c.mult(3,5));
 
 
