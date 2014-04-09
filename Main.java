c21b12814153d63de5e44e43dd7d6a01ec117842
barry528 <smamadou.barry.87@gmail.com>
2014-04-09 12:09:40 +0200

    ko

--- Main.java
+++ Main.java
@@ -1,19 +1,16 @@
 public class Main{
 
 	public static void main(String[] args){
-<<<<<<< HEAD
+
 		public static void main(String[] args){
 		Calc c= new Calc();
 		System.out.print("Salut tout le monde je sais faire des multiplications !");
 		System.out.print("Somme : "+c.mult(3,5));
-		
-	}
-=======
-		Calc c=new Calc();
+
 
 		System.out.println("Somme :"+c.somme(3,5));
 		
->>>>>>> br
+
 		
 	}
 }
